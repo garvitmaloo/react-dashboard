40eea4deeede281983f73fedf5e783ec7217d56b
garvit <garvitmaloo.21@gmail.com>
2023-07-03 07:22:13 +0530

    Including other button attributes

--- src/components/button/IconBtn.tsx
+++ src/components/button/IconBtn.tsx
@@ -3,15 +3,17 @@ import { IconButtonProps } from "../../types/prop_types";
 function IconBtn({
   btnIcon,
   additionalStyles,
-  clickHandler
+  clickHandler,
+  ...rest
 }: IconButtonProps): JSX.Element {
   return (
     <button
       type="button"
-      className={`rounded bg-theme-cyan cursor-pointer p-1 transition-all duration-200 ${
+      className={`rounded bg-theme-cyan cursor-pointer p-1 transition-all duration-200 disabled:cursor-not-allowed ${
         additionalStyles && additionalStyles.join(" ")
       }`}
       onClick={() => clickHandler()}
+      {...rest}
     >
       {btnIcon}
     </button>
