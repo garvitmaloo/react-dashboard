7850f11f433102d0dbe1926d523fee53d0207027
garvit <garvitmaloo.21@gmail.com>
2023-07-06 20:05:36 +0530

    Adding update order status functionality

--- src/pages/OrderDetailsPage.tsx
+++ src/pages/OrderDetailsPage.tsx
@@ -1,7 +1,78 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
+import PrimaryBtn from "../components/button/PrimaryBtn";
+import { OrdersTableData } from "../types/prop_types";
+import { setSnackbarOpen } from "../store/snackbarSlice";
 
 export default function OrderDetails(): JSX.Element {
-  const params = useParams();
+  let orderDetails: OrdersTableData | null = null;
 
-  return <p> Order Details for Order Id = {params.id}</p>;
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const orderDetailsQuery = useQuery({
+    queryKey: ["OrderDetails"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `${process.env.REACT_APP_FIREBASE_DATA_URL}/orders/${
+          +params.id! - 1
+        }.json`
+      );
+      return response.data;
+    }
+  });
+  const updateOrderStatusMutation = useMutation({
+    mutationFn: async () => {
+      const response = await axios.put(
+        `${process.env.REACT_APP_FIREBASE_DATA_URL}/orders/${
+          +params.id! - 1
+        }.json`,
+        { ...orderDetails, orderStatus: "Fulfilled" }
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["OrderDetails"]);
+      dispatch(
+        setSnackbarOpen({
+          isOpen: true,
+          message: "Order status updated successfully"
+        })
+      );
+      navigate("/orders");
+    }
+  });
+  const orderFulfillHandler = () => {
+    updateOrderStatusMutation.mutate();
+  };
+
+  if (orderDetailsQuery.data) {
+    orderDetails = orderDetailsQuery.data;
+  }
+
+  if (orderDetailsQuery.isError) {
+    dispatch(
+      setSnackbarOpen({
+        isOpen: true,
+        message: (orderDetailsQuery.error as any).message
+      })
+    );
+  }
+
+  return (
+    <div>
+      <p> Order Details for Order Id = {params.id}</p>
+      {orderDetails && orderDetails.orderStatus.toLowerCase() === "pending" && (
+        <PrimaryBtn
+          btnText="Mark as Fulfilled"
+          additionalStyles={["bg-theme-yellow", "block", "mx-auto", "my-5"]}
+          clickHandler={orderFulfillHandler}
+        />
+      )}
+    </div>
+  );
 }
