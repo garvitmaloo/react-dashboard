531857eb20ef02af351def5366f2612620597c30
garvit <garvitmaloo.21@gmail.com>
2023-07-06 08:31:47 +0530

    Adding delete product functionality

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
 import BasicModal from "../components/modal/AddProductFormModal";
 import PrimaryBtn from "../components/button/PrimaryBtn";
 import { ProductsDetails } from "../types/prop_types";
+// import PositionedSnackbar from "../components/snackbar/Snackbar";
 
 export default function ProductDetailsPage(): JSX.Element {
   let productDetails = {};
   const params = useParams();
+  const userState = useSelector((state: any) => state.user);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
   const productDetailsQuery = useQuery({
     queryKey: ["Details"],
     queryFn: async () => {
@@ -21,7 +24,14 @@ export default function ProductDetailsPage(): JSX.Element {
       return response.data;
     }
   });
-  const userState = useSelector((state: any) => state.user);
+  const deleteProductMutation = useMutation({
+    mutationFn: async () => {
+      const response = await axios.delete(
+        `${process.env.REACT_APP_FIREBASE_DATA_URL}/products/${params.id}.json`
+      );
+      return response;
+    }
+  });
 
   if (productDetailsQuery.data) {
     productDetails = { ...productDetailsQuery.data, productId: params.id };
@@ -31,18 +41,36 @@ export default function ProductDetailsPage(): JSX.Element {
     setIsModalOpen(true);
   };
 
+  const deleteHandler = () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm("Are you sure you want to delete this product?")) {
+      deleteProductMutation.mutate();
+    }
+  };
+
   const modalCloseHandler = (value: boolean) => {
     setIsModalOpen(value);
   };
+
+  if (deleteProductMutation.isSuccess) {
+    navigate("/products");
+  }
   return (
     <div>
       <p>Product Details for product id = {params.id}</p>
       {userState.role === "Admin" && (
-        <PrimaryBtn
-          btnText="Edit Product"
-          additionalStyles={["block my-4 mx-auto bg-theme-yellow"]}
-          clickHandler={clickHandler}
-        />
+        <div className="flex justify-center items-center gap-5">
+          <PrimaryBtn
+            btnText="Edit Product"
+            additionalStyles={["my-4 bg-theme-yellow"]}
+            clickHandler={clickHandler}
+          />
+          <PrimaryBtn
+            btnText="Delete Product"
+            additionalStyles={["block my-4 bg-theme-orange"]}
+            clickHandler={deleteHandler}
+          />
+        </div>
       )}
 
       {isModalOpen && (
